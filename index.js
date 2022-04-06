2081a5f9c893bccbf3bda7c6a222bd7efd74ca48
Melanie Tong <melaniietong@gmail.com>
2022-04-06 13:38:37 -0700

    create post route for adding items to a cart

--- index.js
+++ index.js
@@ -6,16 +6,29 @@ const pool = require('./db')
 app.use(cors());
 app.use(express.json());
 
+// Add item to cart
+app.post("/items", async(req, res) => {
+    try {
+        const { item_num, user_cookie, options, quantity } = req.body;
+        const newItem = await pool.query(
+            "INSERT INTO items(item_num, user_cookie, options, quantity) VALUES($1, $2, $3, $4) RETURNING *",
+            [item_num, user_cookie, options, quantity]
+        );
+        res.json(newItem.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 // Create an order
 app.post("/orders", async(req, res) => {
     try {
-        console.log(req.body);
         const { submit_time, order_status, is_takeout, delivery_address, instructions, order_name, phone } = req.body;
         const newOrder = await pool.query(
             "INSERT INTO orders(submit_time, order_status, is_takeout, delivery_address, instructions, order_name, phone) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
             [submit_time, order_status, is_takeout, delivery_address, instructions, order_name, phone]
         );
-        res.json(newOrder.rows[0])
+        res.json(newOrder.rows[0]);
     } catch (err) {
         console.error(err.message);
     }
